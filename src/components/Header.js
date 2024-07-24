049f71296df75bdd3bf5d69e0cd8fba21f3957fe
gtarun8341 <gtarun8341@gmail.com>
2024-07-24 12:38:51 +0530

    fouth

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,9 @@ const Header = () => {
       <nav className="flex justify-between items-center">
         {/* Left Section */}
         <div className="flex items-center space-x-4">
-          <img src="/logo.png" alt="Logo" className="w-8 h-8" />
+          <div className="bg-[#0F0F0F] p-1 rounded-full">
+            <img src="/logo.jpg" alt="Logo" className="w-19 h-12" />
+          </div>
           <ul className="flex space-x-4">
             <li>
               <Link href="/pending-pool">
@@ -44,39 +46,39 @@ const Header = () => {
 
         {/* Right Section */}
         <div className="flex items-center space-x-4">
-          <button className="flex items-center justify-between bg-gradient-to-r from-[#00AC82] to-[#271197] backdrop-blur-sm rounded px-4 py-1">
-            <div className="flex flex-col items-start mr-3">
-              <span className="font-bold text-lg text-white">Purchase Premium</span>
-              <span className="text-sm text-white">Pay 0% Fees</span>
-            </div>
-            <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M19.9984 14.2065L1.99736 14.2065L0 4.2501L7.00017 8.25019L11.0003 0.25L15.0004 8.25019L22.0005 4.2501L19.9984 14.2065Z" fill="url(#paint0_linear_2751_154)"/>
-              <path d="M1.99971 15.25H20.0001V18.75H1.99736L1.99971 15.25Z" fill="url(#paint1_linear_2751_154)"/>
-              <defs>
-                <linearGradient id="paint0_linear_2751_154" x1="11.3336" y1="0.249613" x2="10.9996" y2="16.25" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#FFC700"/>
-                  <stop offset="0.901" stopColor="#FFD04A"/>
-                </linearGradient>
-                <linearGradient id="paint1_linear_2751_154" x1="11.3336" y1="0.249613" x2="10.9996" y2="16.25" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#FFC700"/>
-                  <stop offset="0.901" stopColor="#FFD04A"/>
-                </linearGradient>
-              </defs>
-            </svg>
-          </button>
-          <div className="relative flex items-center space-x-2">
-            {/* Image and Balance Container */}
-            <div className="flex items-center space-x-2 border-2 border-white p-1 rounded-full">
-              <div className="text-white text-xl">123</div>
-              <img src="/sol.jpg" alt="Wallet" />
+  <button className="flex items-center justify-between bg-gradient-to-r from-[#00AC82] to-[#271197] backdrop-blur-sm rounded px-4 py-1">
+    <div className="flex flex-col items-start mr-3">
+      <span className="font-bold text-lg text-white">Purchase Premium</span>
+      <span className="text-sm text-white">Pay 0% Fees</span>
+    </div>
+    <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19.9984 14.2065L1.99736 14.2065L0 4.2501L7.00017 8.25019L11.0003 0.25L15.0004 8.25019L22.0005 4.2501L19.9984 14.2065Z" fill="url(#paint0_linear_2751_154)"/>
+      <path d="M1.99971 15.25H20.0001V18.75H1.99736L1.99971 15.25Z" fill="url(#paint1_linear_2751_154)"/>
+      <defs>
+        <linearGradient id="paint0_linear_2751_154" x1="11.3336" y1="0.249613" x2="10.9996" y2="16.25" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FFC700"/>
+          <stop offset="0.901" stopColor="#FFD04A"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_2751_154" x1="11.3336" y1="0.249613" x2="10.9996" y2="16.25" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FFC700"/>
+          <stop offset="0.901" stopColor="#FFD04A"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  </button>
+  <div className="relative flex items-center space-x-2">
+    {/* Image and Balance Container */}
+    <div className="flex items-center space-x-2 border-2 border-white p-1 rounded-full bg-[#0F0F0F] h-10 px-3">
+      <div className="text-white text-xl">123</div>
+      <img src="/sol.jpg" alt="Wallet" className="w-18 h-5"/>
+    </div>
+  </div>
+  <div className="user-id flex items-center space-x-2 border-2 border-white p-1 rounded-full text-white bg-[#0F0F0F] h-10 px-3">123456....34567</div>
+  <div className="profile-icon bg-[#0F0F0F] p-1 rounded-full">
+    <img src="/profile.jpg" alt="Profile Icon" className="w-8 h-8 rounded-full border-white" />
+  </div>
+</div>
 
-            </div>
-          </div>
-          <div className="user-id flex items-center space-x-2 border-2 border-white p-1 rounded-full text-white">123456....34567</div>
-          <div className="profile-icon">
-            <img src="/profile-icon.png" alt="Profile Icon" className="w-6 h-6 rounded-full border-white" />
-          </div>
-        </div>
       </nav>
     </header>
   );
